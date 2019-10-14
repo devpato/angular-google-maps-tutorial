f9c2768e13dc58de194043b1c58fa9d2cc9d9cc1
Patricio Vargas <pevargas2730@gmail.com>
2019-10-13 20:51:19 -0400

    added coordinates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,7 @@ export class AppComponent {
   title = 'angular-gmap';
   @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
   map: google.maps.Map;
+  lat = 40.73061;
+  lng = -73.935242;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
 }
