3afe57afc400cb110f7c0df052691b1ce59d2bc7
Patricio Vargas <pevargas2730@gmail.com>
2019-10-13 21:01:00 -0400

    adding marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,19 @@ export class AppComponent implements AfterViewInit {
   map: google.maps.Map;
   lat = 40.73061;
   lng = -73.935242;
+
   coordinates = new google.maps.LatLng(this.lat, this.lng);
+
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
     zoom: 8
   };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+    title: 'Hello World!'
+  });
 
   ngAfterViewInit() {
     this.mapInitializer();
@@ -23,5 +31,6 @@ export class AppComponent implements AfterViewInit {
 
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
   }
 }
